f9733adcdfdd5813938f182df3bcb9ad7b8b28c5
Solomon Taiwo <95451488+solomontaiwo@users.noreply.github.com>
2022-12-19 17:18:36 +0100

    Updated with comments

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -2,7 +2,11 @@ import pyqrcode
 
 from PIL import Image
 
+# Ask user what the qr code will contain
 link = input("Enter anything to generate QR: ")
+
+# Create qr code based on user input
 qr_code = pyqrcode.create(link)
+
+# Create png file with appropriate filename and scale
 qr_code.png("QRCode_" + link + ".png", scale = 5)
-Image.open("QRCode.png")
